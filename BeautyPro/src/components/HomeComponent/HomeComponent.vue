46e833248c289b22a06b3c7a47655ab53b9cb9ab
LiuLiFei <1061334689@qq.com>
2018-12-10 19:06:09 +0800

    新增个人页面以及路由

---
+++ BeautyPro/src/components/HomeComponent/HomeComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-component">
+
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeComponent",
+      methods:{
+
+      },
+    }
+</script>
+
+<style scoped>
+
+</style>
